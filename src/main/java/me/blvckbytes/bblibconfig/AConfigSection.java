c952cabd2339db06a17ae50fe47cf33f7c4dbd6f
Philip Hell <blvckbytes@gmail.com>
2022-07-24 00:32:34 +0200

    added sane default values for ConfigValue and String to AConfigSection

--- src/main/java/me/blvckbytes/bblibconfig/AConfigSection.java
+++ src/main/java/me/blvckbytes/bblibconfig/AConfigSection.java
@@ -38,6 +38,12 @@ public abstract class AConfigSection {
    * @return Value to use as a default
    */
   public @Nullable Object defaultFor(Class<?> type, String field) {
+    if (type == ConfigValue.class)
+      return ConfigValue.immediate("&cundefined");
+
+    if (type == String.class)
+      return "§cundefined";
+
     return null;
   }
 
