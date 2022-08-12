8c3023ceac3040701cac3b4a6f84380683e6d892
Philip Hell <blvckbytes@gmail.com>
2022-08-12 05:05:21 +0200

    added license

--- src/main/java/me/blvckbytes/bblibconfig/sections/ItemStackBannerPatternSection.java
+++ src/main/java/me/blvckbytes/bblibconfig/sections/ItemStackBannerPatternSection.java
@@ -15,6 +15,19 @@ import java.util.Map;
   Created On: 06/28/2022
 
   Represents a banner pattern set on a banner item stack.
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as published
+  by the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 @Getter
 public class ItemStackBannerPatternSection extends AConfigSection {
