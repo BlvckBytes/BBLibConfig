08ee3181f51911d0a4296bb8652e92630f5c8462
Philip Hell <blvckbytes@gmail.com>
2022-08-13 04:25:50 +0200

    implemented a basic IComponentApplicator which can set displayname and lore on ItemStacks to IComponents

---
+++ src/main/java/me/blvckbytes/bblibconfig/component/ComponentApplicator.java
@@ -0,0 +1,109 @@
+package me.blvckbytes.bblibconfig.component;
+
+import me.blvckbytes.bblibdi.AutoConstruct;
+import me.blvckbytes.bblibdi.AutoInject;
+import me.blvckbytes.bblibreflect.MCReflect;
+import me.blvckbytes.bblibreflect.ReflClass;
+import me.blvckbytes.bblibutil.logger.ILogger;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/*
+  Author: BlvckBytes <blvckbytes@gmail.com>
+  Created On: 08/12/2022
+
+  Reflection based implementation of the component applicator.
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as published
+  by the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+@AutoConstruct
+public class ComponentApplicator implements IComponentApplicator {
+
+  private final Class<?> CRAFT_META_ITEM_C, I_CHAT_BASE_COMPONENT_C;
+  private final MCReflect reflect;
+  private final ILogger logger;
+
+  public ComponentApplicator(
+    @AutoInject MCReflect reflect,
+    @AutoInject ILogger logger
+  ) throws Exception {
+    this.reflect = reflect;
+    this.logger = logger;
+
+    CRAFT_META_ITEM_C = reflect.getClassBKT("inventory.CraftMetaItem");
+    I_CHAT_BASE_COMPONENT_C = reflect.getReflClass(ReflClass.I_CHAT_BASE_COMPONENT);
+  }
+
+  @Override
+  public void setDisplayName(IComponent displayName, ItemStack item) {
+    ItemMeta meta = item.getItemMeta();
+
+    // Meta unavailable
+    if (meta == null)
+      return;
+
+    try {
+      boolean isJson;
+      Field f;
+
+      // Figure out whether there's still a IChatBaseComponent, or already a JSON String
+      try {
+        f = reflect.findFieldByType(CRAFT_META_ITEM_C, String.class, 0);
+        isJson = true;
+      } catch (Exception e) {
+        f = reflect.findFieldByType(CRAFT_META_ITEM_C, I_CHAT_BASE_COMPONENT_C, 0);
+        isJson = false;
+      }
+
+      // Overwrite the displayname string or base component
+      f.set(
+        meta,
+        isJson ?
+          displayName.toJson().toString() :
+          reflect.chatComponentFromJson(displayName.toJson())
+      );
+
+      item.setItemMeta(meta);
+    } catch (Exception e) {
+      logger.logError(e);
+    }
+  }
+
+  @Override
+  public void setLore(List<IComponent> lines, ItemStack item) {
+    ItemMeta meta = item.getItemMeta();
+
+    // Meta unavailable
+    if (meta == null)
+      return;
+
+    try {
+      // Jsonify all lore lines
+      List<String> lore = lines.stream()
+        .map(c -> c.toJson().toString())
+        .collect(Collectors.toList());
+
+      // Overwrite the list ref
+      reflect.findGenericFieldByType(CRAFT_META_ITEM_C, List.class, String.class, 0).set(meta, lore);
+
+      item.setItemMeta(meta);
+    } catch (Exception e) {
+      logger.logError(e);
+    }
+  }
+}
---
+++ src/main/java/me/blvckbytes/bblibconfig/component/IComponentApplicator.java
@@ -0,0 +1,43 @@
+package me.blvckbytes.bblibconfig.component;
+
+import org.bukkit.inventory.ItemStack;
+
+import java.util.List;
+
+/*
+  Author: BlvckBytes <blvckbytes@gmail.com>
+  Created On: 08/13/2022
+
+  The public API which the component applicator offers to other consumers
+  in order to apply the IComponent to as many destinations as possible.
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as published
+  by the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+public interface IComponentApplicator {
+
+  /**
+   * Sets a component as the display name of an item
+   * @param displayName Displayname to set
+   * @param item Item to apply on
+   */
+  void setDisplayName(IComponent displayName, ItemStack item);
+
+  /**
+   * Sets a list of components as the lore of an item
+   * @param lines Lore to set
+   * @param item Item to apply on
+   */
+  void setLore(List<IComponent> lines, ItemStack item);
+
+}
